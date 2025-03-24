1fff9647435377cdb5f9ccb89f22687adffb950c
SMART24\User <sgagjq133@gmail.com>
2025-03-24 16:29:41 +0900

    흑흑

---
+++ lib/setting.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/drawer.dart';
+
+class SettinPage extends StatelessWidget {
+  const SettinPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("설정"),
+      ),
+      drawer: const MyDrawer(),
+      body: const Center(
+        child: Text("설정 페이지"),
+      ),
+    );
+  }
+}
