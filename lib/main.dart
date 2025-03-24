a3eaef9bc9512dd3d77de9425a6dead26cc0106e
SMART24\User <sgagjq133@gmail.com>
2025-03-24 14:21:55 +0900

    그리드

--- lib/main.dart
+++ lib/main.dart
@@ -31,32 +31,47 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Text("안녕하세요")
+      appBar: AppBar(
+        title: const Text("안녕하세요"),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.red,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width /2,
+                  height: 200,
+                  color: Colors.yellow,
+                )
+              ],
+            )
+          ],
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    height: 200,
-                    color: Colors.red,
-                  )
-                ],
-              )
-            ],
-          ),
-        )
+      ),
     );
   }
 }
